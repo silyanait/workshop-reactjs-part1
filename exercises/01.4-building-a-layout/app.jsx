ba29d152c8b0942e74835fd337c62d1dd4073b49
Silya NAIT ZERRAD <silya.nait@epitech.eu>
2022-05-11 16:54:21 +0200

    [ADD]: ex 1, ex2, ex3

--- exercises/01.4-building-a-layout/app.jsx
+++ exercises/01.4-building-a-layout/app.jsx
@@ -14,6 +14,17 @@ const data = {
 
 let content = <img src={data.image} />;
 
+<div class="card m-5">
+	<img class="card-img-top" src="..." alt="Card image cap" />
+	<div class="card-body">
+		<h5 class="card-title">Card title</h5>
+		<p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+		<a href="#" class="btn btn-primary">
+			Go somewhere</a>
+	</div>
+</div>;
+
+
 /**
  * define the variable 'content' here and fill it with the
  * needed code to render the bootstrap card
